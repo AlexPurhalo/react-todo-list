4028c752d1d5cc7f6e45f3e05a7119f8932dd3c3
alexpurhalo <alexpurhalo@gmail.com>
2016-07-14 12:17:07 +0300

    hot-module-reloading

--- components/App.js
+++ components/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 
-
 export default class App extends Component {
     render() {
         return <div>
-            <h1>This is definitely a React app now!</h1>
+            <h1>Works!!!!!!!</h1>
         </div>
     }
 }
